3908b5205925788c775ccfcaab04c8554d43a61c
Dasari Uday <dasari.u@jmangroup.com>
2025-01-24 09:49:05 +0530

    there is a latest update

--- Practice.java
+++ Practice.java
@@ -1,7 +1,9 @@
 import java.util.*;
 class Practice{
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+      //  Scanner sc=new Scanner(System.in);
         System.out.println("Project Begin");
+        System.out.println("removed uneccesary Scanner");
+        System.out.println("project is halfway completed");
     }
 }
